78d3a34d7d46ea24ae2e2278737b27639452a4e4
Oskar Szwajkowski <oskar.szwajkowski@pb.com>
2019-09-15 14:24:47 +0200

    - Activity details view

--- client/pb-beskid-hack/src/app/app-routing.module.ts
+++ client/pb-beskid-hack/src/app/app-routing.module.ts
@@ -1,34 +1,38 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
 
-import { AgmCoreModule } from '@agm/core';
+import {AgmCoreModule} from '@agm/core';
 
-import { MainComponent } from './main/main.component';
-import { EventListComponent } from './event-list/event-list.component';
-import { EventEditComponent } from './event-edit/event-edit.component';
-import { EventsListComponent } from "./events-list/events-list.component";
+import {MainComponent} from './main/main.component';
+import {EventListComponent} from './event-list/event-list.component';
+import {EventEditComponent} from './event-edit/event-edit.component';
+import {SingleActivityComponent} from "./single-activity/single-activity.component";
 
 const routes: Routes = [
-    {
-        path: '',
-        component: MainComponent
-    },
-    {
-        path: 'eventList',
-        component: EventListComponent
-    },
-    {
-        path: 'eventEdit/:id',
-        component: EventEditComponent
-    }
+  {
+    path: '',
+    component: MainComponent
+  },
+  {
+    path: 'eventList',
+    component: EventListComponent
+  },
+  {
+    path: 'eventEdit/:id',
+    component: EventEditComponent,
+  }, {
+    path: 'activity/:id/:number',
+    component: SingleActivityComponent
+  }
 
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes),
-  AgmCoreModule.forRoot({
+    AgmCoreModule.forRoot({
       apiKey: 'AIzaSyDUcBYc-o790SS7zrViOsnk-3oGBbgJdmo'
     })],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- client/pb-beskid-hack/src/app/events.service.ts
+++ client/pb-beskid-hack/src/app/events.service.ts
@@ -3,6 +3,7 @@ import {environment} from "../environments/environment";
 import {Observable} from "rxjs";
 import {HttpClient} from "@angular/common/http";
 import {Activity, BeskidEvent, BeskidEventDetails} from "./interfaces/interfaces";
+import {map} from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root'
@@ -19,6 +20,22 @@ export class EventsService {
   }
 
   public getEventDetails(name: string): Observable<BeskidEventDetails> {
-    return this.http.get<BeskidEventDetails>(`${this.URL}/${name}`);
+    return this.http.get<BeskidEventDetails>(`${this.URL}/${name}`)
+      .pipe(
+        map(event => {
+          return {...event, steps: event.steps.sort(this.activitySorter)};
+        })
+      );
+  }
+
+  private activitySorter(a: Activity, b: Activity): number {
+    const same = a.status === b.status;
+    if (same) {
+      return a.name.localeCompare(b.name);
+    } else if (a.status === "COMPLETED") {
+      return 1;
+    } else if (b.status === "COMPLETED") {
+      return -1;
+    }
   }
 }
